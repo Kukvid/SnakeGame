c6dea710989a9b2dfed92a84fb35d02c57d63a72
Kukvid <daniilminakov579@gmail.com>
2023-05-14 23:43:34 +0300

    Version 2.0

--- main.py
+++ main.py
@@ -1,68 +1,89 @@
-import winsound
-from turtle import Screen
-from snake import Snake
-from food import Food
-from scoreboard import Scoreboard
-import time
+import random
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.title("Змейка от Даниила Минакова")
-screen.colormode(255)
-screen.bgcolor(194, 178, 128)
-screen.tracer(0)
+import pygame
 
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
+pygame.init()  # Инициализируем встроенные в библиотеку модули
 
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
-WIN_SCORE = 25
+# Задаем параметры экрана и игры
+WIDTH, HEIGHT = 720, 480
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Snake by Minakov Daniil")
+
+fps = 5
+RECT_SIZE = 30
+MAP_SIZE = WIDTH // RECT_SIZE, HEIGHT // RECT_SIZE
+clock = pygame.time.Clock()
+
+pygame.font.init()
+font_score = pygame.font.SysFont("Arial", 25)
+font_gameover = pygame.font.SysFont("Arial", 45)
+font_space = pygame.font.SysFont("Arial", 18)
+
+# Задаем начальные параметры змейки и яблока
+snake = [((MAP_SIZE[0] // 2) - 2, MAP_SIZE[1] // 2), ((MAP_SIZE[0] // 2) - 1, MAP_SIZE[1] // 2), (MAP_SIZE[0] // 2, MAP_SIZE[1] // 2)]
+
+direction = 0
+directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+apple_pos = random.randint(0, MAP_SIZE[0] - 1), random.randint(0, MAP_SIZE[1] - 1)
+
+# Запускаем цикл с игрой
+game_active = True
+while True:
+    screen.fill('#e1bf92')  # Заполняем экран цветом
+
+    # Проверяем все события, которые может создать пользователь
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT and direction != 2:
+                direction = 0
+            if event.key == pygame.K_LEFT and direction != 0:
+                direction = 2
+            if event.key == pygame.K_UP and direction != 1:
+                direction = 3
+            if event.key == pygame.K_DOWN and direction != 3:
+                direction = 1
+            if event.key == pygame.K_SPACE and game_active == False:
+                game_active = True
+                snake = [((MAP_SIZE[0] // 2) - 2, MAP_SIZE[1] // 2), ((MAP_SIZE[0] // 2) - 1, MAP_SIZE[1] // 2), (MAP_SIZE[0] // 2, MAP_SIZE[1] // 2)]
+                apple_pos = random.randint(0, MAP_SIZE[0] - 1), random.randint(0, MAP_SIZE[1] - 1)
+                direction = 0
+                fps = 5
+
+    # Выведем змейку и яблоко на экран
+    [pygame.draw.rect(screen, 'green', (pos[0] * RECT_SIZE, pos[1] * RECT_SIZE, RECT_SIZE, RECT_SIZE)) for pos in snake]
+    pygame.draw.rect(screen, 'blue', (apple_pos[0] * RECT_SIZE, apple_pos[1] * RECT_SIZE, RECT_SIZE, RECT_SIZE))
+
+    # Логика движения змейки
+    if game_active:
+        new_pos = snake[-1][0] + directions[direction][0], snake[-1][1] + directions[direction][1]
+        if not (0 <= new_pos[0] < MAP_SIZE[0] and 0 <= new_pos[1] < MAP_SIZE[1]) or \
+                new_pos in snake:
+            game_active = False
+        else:
+            snake.append(new_pos)
+            if new_pos == apple_pos:
+                fps += 1
+                while apple_pos in snake:
+                    apple_pos = random.randint(0, MAP_SIZE[0] - 1), random.randint(0, MAP_SIZE[1] - 1)
+            else:
+                snake.pop(0)
+    else:
+        white_screen = pygame.Surface((720, 480), pygame.SRCALPHA)
+        white_screen.fill((255, 255, 255, 128))
+        screen.blit(white_screen, (0, 0))
+
+        gameover_text = font_gameover.render(f"GAME OVER", 1, "black")
+        screen.blit(gameover_text, (WIDTH // 2 - gameover_text.get_width() // 2, HEIGHT // 2 - 50))
+        replay_text = font_space.render("Press SPACE to replay", 1, "black")
+        screen.blit(replay_text, (WIDTH // 2 - replay_text.get_width() // 2, HEIGHT // 2 + 10))
 
 
-# Экран-заставка
-# character_pen = Character
-def game():
-    winsound.PlaySound("Electro-Light.wav",winsound.SND_NODEFAULT|winsound.SND_ASYNC|winsound.SND_LOOP)
-    game_is_on = True
-    # Запускаем процесс игры
-    while game_is_on:
-        # snake.move()
-        time.sleep(0.16)
-        if scoreboard.score == WIN_SCORE:
-            game_is_on = False
-            scoreboard.win()
-        # Detect collision with food.
-        if snake.head.distance(food) < 15:
-            snake.extend()
-            scoreboard.increase_score()
-            if game_is_on:
-                food.refresh()
-            else: food.delete()
-
-        # Detect collision with wall.
-        if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-            game_is_on = False
-            snake.dead_snake(screen)
-            scoreboard.game_over()
-
-        # Detect collision with tail.
-        for segment in snake.segments:
-            if segment == snake.head:
-                pass
-            elif snake.head.distance(segment) < 10:
-                game_is_on = False
-                snake.dead_snake(screen)
-                scoreboard.game_over()
-        screen.update()
-        if game_is_on:
-            snake.move()
-
-
-game()
-# screen.exitonclick()
-screen.mainloop()
+    score_text = font_score.render(f"Score: {len(snake)}", 1, "red")
+    screen.blit(score_text,(5,5))
+    pygame.display.update()
+    clock.tick(fps)
